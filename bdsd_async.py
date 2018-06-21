16b09fa083fab0416f88df45a87ad3cd43cf9a60
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2018-06-22 02:29:25 +0300

    async works

--- bdsd_async.py
+++ bdsd_async.py
@@ -23,8 +23,30 @@ class BDSDClient(asyncore.dispatcher, EventEmitter):
     def handle_close(self):
         self.close()
 
+    def _handleIncomingNotify(self, response):
+        payload = response['payload']
+        if payload == 'bus connected':
+            self.emit('connected')
+    def _handleIncomingValue(self, response):
+        payload = response['payload']
+        self.emit('value', payload)
+    def _handleIncomingResponse(self, response):
+        # TODO: find origin request and callback
+        pass
+
     def handle_read(self):
-        print(self.recv(8192))
+        data = self.recv(8192)
+        print(data)
+        parsed = _bdsm.parse(data)
+        print(parsed[2])
+        response = json.loads(parsed[2])
+        method = response['method']
+        if method == 'notify':
+            self._handleIncomingNotify(response)
+        elif method == 'cast value':
+            self._handleIncomingValue(response)
+        else:
+            self._handleIncomingResponse(response)
 
     # def writable(self):
     #     return
@@ -32,8 +54,50 @@ class BDSDClient(asyncore.dispatcher, EventEmitter):
     def handle_write(self):
         sent = self.send('hello')
 
+    # BDSD Client methods
+    def _sendData(self, data):
+        msg = _bdsm.compose(data)
+        self.send(msg)
 
-client = BDSDClient('/run/user/1000/bdsd.sock')
+    def _composeDataStr(self, method, payload):
+        data = {}
+        data['request_id'] = round(random.random() * 1000)
+        data['method'] = method
+        data['payload'] = payload
+        return data
+
+    def setValue(self, id, value, cb):
+        # define request
+        print('set value')
+        method = 'set value'
+        payload = {}
+        payload['id'] = id
+        payload['value'] = value
+        # compose and send
+        msg = self._composeDataStr(method, payload)
+        # request = msg['request_id'], cb
+        # self._requests.append(request)
+        # print(request)
+        # print(request)
+        # TODO: register callback in self._requests
+        msgStr = json.dumps(msg)
+        self._sendData(msgStr)
+
+SOCKFILE = os.environ.copy()['XDG_RUNTIME_DIR'] + '/bdsd.sock'
+myClient = BDSDClient(SOCKFILE)
 print('starting async')
-asyncore.loop()
 
+
+@myClient.on('connected')
+def handle_connected():
+    print('myClient is connected, great then')
+    myClient.setValue(103, False, 1)
+
+
+@myClient.on('value')
+def handle_broadcasted_value(data):
+    print('broadcasted value')
+    print(data)
+
+
+asyncore.loop()
